1fc2733c1a1c38fc539d73705f6e1053028fdbe7
cwadani75 <cwadani75@gmail.com>
2025-04-22 12:14:17 +0300

    hooks-list

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,14 @@
-import React from "react";
+function ProjectItem({ name, about, technologies, image }) {
+  const techSpans = technologies.map((tech) => (
+    <span key={tech}>{tech}</span>
+  ));
 
-function ProjectItem({ name, about, technologies }) {
   return (
     <div className="project-item">
+      <img src={image} alt={name} />
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies">{techSpans}</div>
     </div>
   );
 }
